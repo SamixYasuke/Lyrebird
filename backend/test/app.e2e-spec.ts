866e8109ed2a82fdc07c1e68ba5b4b1b4e88ba60
SamixYasuke <samueladekolu4@gmail.com>
2026-08-08 20:43:51 +0100

    first commit

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -6,7 +6,6 @@ import { AppModule } from '@/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
-
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
